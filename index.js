3a5f1c9dc2e5b6bd70b0b38842e94e655146be3a
Munguunuu <munguunuu@Munguunuus-MacBook-Air.local>
2025-05-20 16:50:26 +0800

    fix some

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const http = require('http');
 const PORT = process.env.PORT || 3000;
+
 http.createServer((req, res) => {
   res.end('Hello from Jenkins CI/CD!');
-}).listen(PORT, () => {
+}).listen(PORT, '0.0.0.0', () => {
   console.log(`Running on port ${PORT}`);
 });
